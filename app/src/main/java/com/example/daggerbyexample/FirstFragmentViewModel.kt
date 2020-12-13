c17fa17193a82776c7c624f647f33c070aebee67
phamhoaithanh <pham54833@gmail.com>
2020-12-13 14:55:55 +0700

    push code

--- app/src/main/java/com/example/daggerbyexample/FirstFragmentViewModel.kt
+++ app/src/main/java/com/example/daggerbyexample/FirstFragmentViewModel.kt
@@ -6,7 +6,7 @@ import androidx.lifecycle.liveData
 import kotlinx.coroutines.delay
 import javax.inject.Inject
 
-class FirstFragmentViewModel @Inject constructor(): ViewModel() {
+class FirstFragmentViewModel @Inject constructor() : ViewModel() {
     private val _infoText = liveData {
         while (true) {
             emit(System.currentTimeMillis().toString())
